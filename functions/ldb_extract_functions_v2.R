8fba07e8eb4f7ee3556e14d1fc870f57173641c8
John Yuill <john@fig4.com>
2024-12-09 01:11:30 -0800

    basic functionality for pdf-scraping returned - but appears to be missing some data. spirits most likely.

--- functions/ldb_extract_functions_v2.R
+++ functions/ldb_extract_functions_v2.R
@@ -77,6 +77,7 @@ fn_lmr <- function(furl, rotate){
     #info <- pdf_info(pdf_file) # get metadata: # of pgs, title, etc
     # convert pdf to readable format in R and import
     if(rotate==TRUE){
+      cat("start rotate pdf pages \n")
       #lmr <- pdf_text(pdf_file) # original process - before LDB reformatted
       # setup for separate file for rotated version, if needed (testing?)
       #output_pdf_file <- paste0(input_folder,"rotated_",fname)
@@ -87,6 +88,7 @@ fn_lmr <- function(furl, rotate){
   }
     
   # OCR conversion ----
+  cat("start OCR conversion \n")
   # convert rotated pdf from images to text (needed starting Sep 2024)
   # complicated to save result as pdf, so just running every time
   lmr_ocr <- tesseract::ocr(pdf_file)
@@ -100,12 +102,14 @@ fn_lmr <- function(furl, rotate){
   # leaving option open for other proc if pdf format changes after Sep 2024
   lmr_use <- lmr_ocr
   return(list(lmr_use, fname))
+  cat('PDF import/process complete \n')
 }
 
-## clean pg -> remove summary, blank rows
+## clean pg -> remove summary, blank rows, etc
 fn_pg_clean <- function(tbl_pg_rows) {
   ## > RM SUMMARY ROWS (or blank) ####
-  cat("clean tbl: remove summary, blank rows \n")
+  cat(paste0("page cleaning: ",length(tbl_pg_rows), " rows total \n"))
+  # check for and remove unwanted rows
   srows <- NULL
   for(r in 1:length(tbl_pg_rows)){
     if(str_detect(tbl_pg_rows[r],"Summary")==TRUE | 
@@ -113,17 +117,25 @@ fn_pg_clean <- function(tbl_pg_rows) {
        str_detect(tbl_pg_rows[r],"Liquor Market Review") |
        str_detect(tbl_pg_rows[r], "TOP")){
       srow <- r
-      srows <- c(srows,srow)
+      srows <- c(srows,srow) # accumulate rows to remove
     }
   }
   ## want to remove Summary OR blank rows
+  if(length(srows)==0){
+    cat("no summary or blank rows to remove \n")
+  } else {
+    cat("removing rows: ", srows,"\n")
+  }
   tbl_pg_rows_ns <- tbl_pg_rows[c(-srows)]
+  cat(paste0(length(tbl_pg_rows_ns), " rows after cleaning \n"))
+  print(tbl_pg_rows_ns)
   return(tbl_pg_rows_ns)
 }
 
 ## get meta data from pg
 fn_pg_meta <- function(tbl_pg_rows, pg_num, tbl_name_prev, tbl_cat_type){
   ## > get META data ####
+  cat("get META data \n")
   tbl_name <- trimws(tbl_pg_rows[1])
   
   tbl_name_clean <- str_replace_all(tbl_name,"\\(","")
@@ -131,19 +143,23 @@ fn_pg_meta <- function(tbl_pg_rows, pg_num, tbl_name_prev, tbl_cat_type){
   tbl_name_clean <- str_replace_all(tbl_name_clean,"\\$","")
   tbl_name_clean <- trimws(tbl_name_clean, "right")
   tbl_name_clean <- str_replace_all(tbl_name_clean," ","_")
-  cat("pg:",pg_num,'tbl:', tbl_name_clean,"\n")
-  ## check for prev same table name -> if so, keep using prev, since indicates continuation
+  cat("pg:",pg_num,', tbl:', tbl_name_clean,", tbl prev:",tbl_name_prev,"\n")
+  ## check for prev same table name -> if so, 
+  #. - keep using prev, since indicates continuation
   if(exists('tbl_name_prev')){
-    if(tbl_name_clean==tbl_name_prev){
+    if(tbl_name_clean==tbl_name_prev){ ## continuation of table
       tbl_cat_type <- tbl_cat_type
-      hrow <- 2
+      hrow <- 2 # row 2 when page is continuation of table on prev pg
+      tbl_cont <- TRUE
     } else { ## 
       tbl_cat_type <- tbl_pg_rows[2]
       hrow <- 3
+      tbl_cont <- FALSE
     }
   } else {
     tbl_cat_type <- tbl_pg_rows[2]
     hrow <- 3
+    tbl_cont <- FALSE
   }
   ## get table metric
   # this no longer works with OCR versions (Sep 2024)
@@ -160,31 +176,34 @@ fn_pg_meta <- function(tbl_pg_rows, pg_num, tbl_name_prev, tbl_cat_type){
     tbl_metric=='Net Sales $' ~ 'netsales',
     tbl_metric=='Litres' ~ 'litres'
   )
-  cat('metric:',tbl_metric,"\n")
+  cat('hrow:',hrow,'; tbl_cat_type:',tbl_cat_type,'; tbl_metric:',tbl_metric,'; tbl_cont?',tbl_cont,"\n")
   
-  return(list(tbl_name_clean, hrow, tbl_cat_type, tbl_metric))
-}
+  return(list(tbl_name_clean, hrow, tbl_cat_type, tbl_metric, tbl_cont))
+} # end meta data
 
-## extract tbl content from pg
+## extract tbl content pg by pg in loop
 fn_tbl_content <- function(tbl_pg_rows, tbl_meta, categories){
-  cat("start content process function \n")
+  ## >> start content process function ----
+  cat("fn_tbl_content: start content process function \n")
   ## unpack meta data
   tbl_name <- tbl_meta[[1]]
   hrow <- tbl_meta[[2]] ## identifies heading row, depends on pg layout; -1 for blank row
   tbl_cat_type <- tbl_meta[[3]]
   tbl_metric <- tbl_meta[[4]]
-
-  # filter categories for cat type
-  categories <- categories %>% filter(cat_type==tbl_cat_type)
   
+  # >> show meta data ----
+  cat(paste0('tbl_name: ',tbl_name,'; ', 'tbl_cat_type: ',tbl_cat_type,'; ', 
+             'tbl_metric: ',tbl_metric,'; hrow: ',hrow,'\n'))
+
   ## > TABLE + HEADING ####
+  ## >> get headings, set up table ----
   cat("get headings, set up table \n")
   #tbl_heading <- unlist(str_split(tbl_pg_rows[hrow], "\\s{2,}"))[c(-1,-2)] # drop first two: empty, metric name
   # ocr version: split on 1 or more spaces
   # need to drop everything before the first 'Fiscal'
   # then split by occurences of 'Fiscal' and do some cleaning
   tbl_heading <- unlist(str_split(tbl_pg_rows[hrow], "\\s{1,}"))
-  cat("raw heading:",tbl_heading,"\n")
+  #cat("raw heading:",tbl_heading,"\n")
   
   # determine headings - always same structure but need to detect the right Quarters
   # get starting point for each occurence of 'Fiscal'
@@ -204,7 +223,7 @@ fn_tbl_content <- function(tbl_pg_rows, tbl_meta, categories){
     h_all <- c(h_all, h_next)
     #cat(h_all)
   }
-  cat("headings: ",h_all,"\n")
+  #cat("headings: ",h_all,"\n")
   h_all <- str_replace(h_all, "Fiscal ", "FY")
   ## remove two digits for prev yr + '/' ("21/" in example above)
   tbl_heading <- str_replace_all(str_remove(h_all, str_sub(h_all, start=5, end=7))," ","")
@@ -212,6 +231,7 @@ fn_tbl_content <- function(tbl_pg_rows, tbl_meta, categories){
   # add standard cols to headings
   first_cols <- c("cat_type","category","subcategory")
   tbl_heading <- c(first_cols, tbl_heading)
+  ## >> final heading ----
   cat("final heading:",tbl_heading,"\n")
   
   ## initiate empty table with proper headings
@@ -220,85 +240,135 @@ fn_tbl_content <- function(tbl_pg_rows, tbl_meta, categories){
   
   # > PROC ROW DATA ####
   ## get data from each row and add to table
+  ## >> process data ----
   cat("process data \n")
   start <- hrow+1
-  ## loop through each row to collect data
-  for(r in start:length(tbl_pg_rows)){
-    ## pre-defined tbl will accumulate rows through loop
-    cat("table row: ", r, "; ")
-    # original pre Sep 2024 version: split on 2 or more spaces
-    #row_content <- unlist(str_split(trimws(tbl_pg_rows[r]), "\\s{2,}"))
-    # col_nums <- length(row_content)
-    # cat("col nums:", col_nums, "\n")
-    # if(col_nums>5){ ## only rows with data
-    #   tr <- r-hrow ## set table row number by subtract pdf start num
-    #   tbl[tr,"cat_type"] <- tbl_cat_type
-    #   if(col_nums==7){ ## rows with category shown
-    #     tbl$category[tr] <- row_content[1]
-    #     tbl$subcategory[tr] <- row_content[2]
-    #     tbl[tr,c(4:(col_nums+1))] <- row_content[c(3:col_nums)]
-    #   } else {
-    #     ## get category from prev row
-    #     tbl$category[tr] <- tbl$category[tr-1]
-    #     tbl$subcategory[tr] <- row_content[1]
-    #     tbl[tr,c(4:(col_nums+2))] <- row_content[c(2:col_nums)]
-    #   }
-    # end original pre Sep 2024 version
-    # new process as of Sep 2024 based on OCR of tables
-    row_content <- tbl_pg_rows[r] 
-    # split version for detecting number values
-    row_content_split <- unlist(str_split(trimws(tbl_pg_rows[r]), "\\s{1,}"))
-    # get number values starting from end, working back (accounts for variable length)
-    # - clean up text convert to number format
-    ttl_length <- length(row_content_split)
-    num_values <- row_content_split[(ttl_length-4):ttl_length]
-    num_values <- str_remove_all(num_values,"\\$")
-    num_values <- str_remove_all(num_values, ",")
-    num_values <- as.numeric(num_values)
-    trow <- r-hrow # get row number based on r minus hrow value to start at 1
-    tbl[trow, "cat_type"] <- tbl_cat_type
-    # loop through categories table to find match for subcategory
-    # - on subcategory match, set category and subcategory columns
-    # - works for rows with no category available
-    # - possible issue: if categories change in new data, will match with outdated category
-      for(sc in 1:nrow(categories) ) {
-        if(str_detect(row_content, categories$subcategory[sc])) {
-          tbl[trow,"category"] <- categories$category[sc]
-          tbl[trow,"subcategory"] <- categories$subcategory[sc]
-        }
-    # add number values in each col, based on values obtained above
-      tbl[trow,4:8] <- num_values
-      } # end category / subcategory
+  ## confirm that start is less than total rows -> data is available
+  if(start<length(tbl_pg_rows)){
+    # process data rows
+    ## loop through each row to collect data - if more rows beyond start
+    for(r in start:length(tbl_pg_rows)){
+      ## pre-defined tbl will accumulate rows through loop
+      cat("tbl row: ", r, "of ", length(tbl_pg_rows))
+      # original pre Sep 2024 version: split on 2 or more spaces
+      #row_content <- unlist(str_split(trimws(tbl_pg_rows[r]), "\\s{2,}"))
+      # col_nums <- length(row_content)
+      # cat("col nums:", col_nums, "\n")
+      # if(col_nums>5){ ## only rows with data
+      #   tr <- r-hrow ## set table row number by subtract pdf start num
+      #   tbl[tr,"cat_type"] <- tbl_cat_type
+      #   if(col_nums==7){ ## rows with category shown
+      #     tbl$category[tr] <- row_content[1]
+      #     tbl$subcategory[tr] <- row_content[2]
+      #     tbl[tr,c(4:(col_nums+1))] <- row_content[c(3:col_nums)]
+      #   } else {
+      #     ## get category from prev row
+      #     tbl$category[tr] <- tbl$category[tr-1]
+      #     tbl$subcategory[tr] <- row_content[1]
+      #     tbl[tr,c(4:(col_nums+2))] <- row_content[c(2:col_nums)]
+      #   }
+      # end original pre Sep 2024 version
+      # new process as of Sep 2024 based on OCR of tables
+      row_content <- tbl_pg_rows[r] 
+      #cat("row content:", row_content, "\n")
+      # >> known issues in row ----
+      # fix known issues that have been identified 
+      # - such as OCR not recognizing characters correctly
+      #   - will result in NA cat/subcat values in table
+      cat(" if errors, fix known issues in row \n")
+      row_content <- case_when(
+        str_detect(row_content, "lrish") ~ str_replace(row_content, "lrish", "Irish"),
+        str_detect(row_content, "ltal") ~ str_replace(row_content, "ltal", "Ital"),
+        TRUE ~ row_content
+      )
+      # >> content split ====
+      # split version for detecting number values
+      row_content_split <- unlist(str_split(trimws(tbl_pg_rows[r]), "\\s{1,}"))
+      cat("content split:", row_content_split, "\n")
+      # get number values starting from end, working back (accounts for variable length)
+      # - clean up text convert to number format
+      # >> confirm content ----
+      # - confirm row has content before moving ahead
+      if(length(row_content_split)>4) {
+        ttl_length <- length(row_content_split)
+        num_values <- row_content_split[(ttl_length-4):ttl_length]
+        num_values <- str_remove_all(num_values,"\\$")
+        num_values <- str_remove_all(num_values, ",")
+        num_values <- as.numeric(num_values)
+        trow <- r-hrow # get row number based on r minus hrow value to start at 1
+        tbl[trow, "cat_type"] <- tbl_cat_type
+        
+        # >> cat/subcat matching ----
+        # use category tbl provided and loop through to find match for subcategory
+        # - on subcategory match, set category and subcategory columns
+        # - works for rows with no category available
+        # - possible issue: if categories change in new data, will match with outdated category
+        for(sc in 1:nrow(categories) ) {
+          if(str_detect(row_content, categories$subcategory[sc])) {
+            tbl[trow,"category"] <- categories$category[sc]
+            tbl[trow,"subcategory"] <- categories$subcategory[sc]
+          }
+          # append number values in each col, based on values obtained above
+          tbl[trow,4:8] <- num_values
+        } # end category / subcategory
+      } else { 
+        tbl <- NULL
+        tbl_long <- NULL
+      } # end content test
+      
     } ## end row conditional
-    print(tbl)
-  } ## end single page loop
-  
-  ## TIDY FORMAT ####
-  # check for NA within each tbl row? 
-  # not sure what this does so setting it to max NAs per row less than 2
-  if(max(rowSums(is.na(tbl)))<2){
-    tbl_long <- fn_tidy_structure(tbl, tbl_metric)
-  } else {
+    } else {
+    cat("no data rows to process \n")
+    tbl <- NULL
     tbl_long <- NULL
-  }
+  } # end start row check condition
   
-  ## save table name for comparison with next table
-  ## - in some cases, tables run over multiple pages
-  tbl_name_prev <- tbl_name
+    print(tbl)
+    
+    # check for null before proceeding
+    cat(paste0("proceed with final process, return data? ",!is.null(tbl),"\n"))
+    if(!is.null(tbl)){
+      
+      ## > SAVE current page ----
+      # >> save tbl for troubleshoot ----
+      # as temp version troubleshooting (overwrite each time)
+      write_csv(tbl, "lmr-get-update/output/temp.csv")  
+    
+      ##> TIDY FORMAT ####
+      ## >> over to fn_tidy_structure process for tbl_long ----
+      # check for NA within each tbl row, as these may cause trouble; setting to max NAs per row less than 2
+      if(max(rowSums(is.na(tbl)))<2){
+        tbl_long <- fn_tidy_structure(tbl, tbl_metric)
+        } else {
+          cat("TABLE HAS NAs - investigate! \n")
+          tbl_long <- NULL
+        }
   
-  ## return tbl: wide and long
-  return(list(tbl, tbl_long))
+      ## save table name for comparison with next table
+      ## - in some cases, tables run over multiple pages
+      #  - doesn't do anything because not passed back
+      tbl_name_prev <- tbl_name
+      
+      ## return tbl: wide and long
+      return(list(tbl, tbl_long))
+    } # end of null check for tbl
 } ## end single page process
 
-## extend to long format
+## extend to long format - called from within fn_tbl_content above
 fn_tidy_structure <- function(tbl, tbl_metric){
+  # troubleshooting
+  #tbl <- read_csv("lmr-get-update/output/temp.csv")
+  #tbl_metric <- tbl_meta[[4]]
+  ## >> tidy process for tbl_long ----
   cat("tidy process for tbl_long \n")
+  cat(tbl_metric,"\n")
   ## > convert to long ####
   ## convert table with metrics in multiple rows to tidy format
   tbl_long <- tbl %>% pivot_longer(cols=c(4:ncol(tbl)), names_to='period', values_to=tbl_metric)
-  tbl_long <- tbl_long %>% mutate(
-    !!tbl_metric := parse_number(!!sym(tbl_metric))
-  )
+  # used in original process based on LMR format before Sep 2024
+  #tbl_long <- tbl_long %>% mutate(
+  #  !!tbl_metric := parse_number(!!sym(tbl_metric))
+  #)
   tbl_long
 }
 
--- lmr-get-update/LMR-01-fetch-process-all_v4.R
+++ lmr-get-update/LMR-01-fetch-process-all_v4.R
@@ -23,9 +23,6 @@ rm(list=ls())
 furl <- "https://www.bcldb.com/files/Liquor_Market_Review_F24_25_Q2_September_2024_0.pdf"
 ## > rest of process is automated to end > run via 'Source'
 
-## Load functions ####
-source("functions/ldb_extract_functions_v2.R")
-
 ## PROCESS DESCR. ####
 ## LDB QMR has pages with single table per page
 ## - by category, separate table for net sales $ and litres
@@ -46,6 +43,8 @@ source("functions/ldb_extract_functions_v2.R")
 ## 7. Upload to database -> delete any existing rows for period and overwrite with latest data.
 
 ## PROCESS START ####
+## Load functions ####
+source("functions/ldb_extract_functions_v2.R")
 
 ## 2. IMPORT PDF ####
 ## Function to:
@@ -54,17 +53,37 @@ source("functions/ldb_extract_functions_v2.R")
 #    - indicate if rotation of pages needed (starting Sep 2024, pages are landscape format)
 #    - if rotation, png will be created for each pg as by-product; will be deleted at end
 lmr <- fn_lmr(furl, rotate=TRUE)
-# used to get name for saving files, in form: LMR_20XX_0X_FYXXQX; passed back with fn_lmr funct
+# used to get name for saving files, in form: LMR_20XX_0X_FYXXQX; passed back with fn_lmr fn
 lmr_name <- unlist(lmr[2]) 
 lmr_name_clean <- str_remove(lmr_name,"\\.pdf")
-lmr <- unlist(lmr[1]) # report content is passed back with fn_url function
-
+lmr <- unlist(lmr[1]) # report content is passed back with fn_lmr function
+cat(paste0(lmr_name,' import/process complete \n'))
 ## - see 'pdftools-explore.R' for different ways to access page info
 ## > get report meta info ####
 #title_pg <- unlist(strsplit(lmr[1], "\n"))
 #title_pg_dt <- str_replace(trimws(title_pg[8])," ","_")
 
-## 3. COLLECT EACH tbl data ####
+## 2a. Categories ####
+### categories ----
+# get list of categories and subcategories for matching with row content
+# use existing data file to get category type, categories and subcategories
+# - needed to determine how to separate rows into cols, since no other consistent markings
+# - pre-Sep-2024 could use double spaces (commented out code remains below)
+# get list of existing files to filter for most recent
+data_files <- list.files(here('lmr-get-update','output'))
+data_files_info <- file.info(here('lmr-get-update','output',data_files))
+data_files_info$file <- row.names(data_files_info)
+data_file <- data_files_info %>% select(file, mtime) %>% 
+  filter(str_detect(file,"LMR")) %>% filter(mtime == max(mtime))
+# import most recent data file to get cat / subcat
+cat_subcat <- read_csv(data_file$file)
+# summarize by unique cat_type/category/subcategory 
+cat_subcat_all <- cat_subcat %>% group_by(cat_type, category, subcategory) %>%
+  summarize(number=n()) %>% ungroup() %>% select(-number)
+# save category reference if needed for reference
+write_csv(cat_subcat_all, here('lmr-get-update','output','cat_subcat_all.csv'))
+
+## 3. GET EACH tbl (page) data ####
 ## Look at each page to determine which ones have tables
 ## can skip the first 3 pgs - always cover, toc, intro
 ## > netsales df and litres df ####
@@ -72,8 +91,8 @@ tables_all_netsales <- data.frame()
 tables_all_litres <- data.frame()
 tbl_name_prev <- "" ## set start prev pg name, used for tables that continue over pgs 
 tbl_cat_type <- ""
-## > loop through each pg ####
-## START LOOP
+## > START LOOP thru ea pg to get data ####
+cat('Starting page loop \n')
 for(p in 4:length(lmr)){
   ## > identify/skip non-tbl pgs
   ## test for 'Item Subcategory' or 'Glossary' -> identifies chart pages and Glossary pg for exclusion
@@ -86,7 +105,7 @@ for(p in 4:length(lmr)){
       ## > get pg content from PDF ####
       tbl_pg <- lmr[p]
       tbl_pg_rows_init <- unlist(strsplit(tbl_pg, "\n"))
-      ## clean pg -> remove summary & blank rows
+      ## clean pg -> remove summary, blank or other 'noise' rows
       tbl_pg_rows <- fn_pg_clean(tbl_pg_rows_init)
       
       ## > get meta data ####
@@ -96,48 +115,60 @@ for(p in 4:length(lmr)){
       # set current name to prev to handle multi-pg tables
       tbl_name_prev <- tbl_name_clean
       tbl_cat_type <- tbl_meta[[3]]
+      tbl_cont <- tbl_meta[[5]] # to identify if continuation of prev page table
       
-      ## > PROCESS tbl ####
+      # > category setup ----
+      # filter list of categories for current cat_type, pass to function for tbl processing
+      cat_subcat <- cat_subcat_all %>% filter(cat_type==tbl_meta[[3]])
+      
+      ## > PROCESS tbl on each page ####
       ## process content - pass in page content in rows, along with meta data
       # breaks out text rows to cols for table
-      ### categories ----
-      # first get list of categories and subcategories for matching with row content
-      # import table categories and subcategories for matching with new report content
-      # - needed to determine how to separate rows into cols, since no other consistent markings
-      # - pre-Sep-2024 could use double spaces (commented out code remains below)
-      # get list of existing files to filter for most recent
-      data_files <- list.files(here('lmr-get-update','output'))
-      data_files_info <- file.info(here('lmr-get-update','output',data_files))
-      data_files_info$file <- row.names(data_files_info)
-      data_file <- data_files_info %>% filter(mtime == max(mtime))
-      # import most recent data file to get cat / subcat
-      cat_subcat <- read_csv(data_file$file)
-      cat_subcat <- cat_subcat %>% group_by(cat_type, category, subcategory) %>%
-        summarize(number=n())
-      ### process rows ----
+      
+      ### process rows ea pg ----
+      # get back table of data for each pg: original + long version
       page_data_tbls <- fn_tbl_content(tbl_pg_rows, tbl_meta, cat_subcat)
-
-      ## > SAVE: wide, long ####
-      ## save results for page - identifying report by name and table -> wide and long versions
-      ## confirm that table returned has data -> any more than 1 NA in first row, skip it
-      if(rowSums(is.na(page_data_tbls[[1]][1,]))<2){
-        ## as shown in report
-        tbl_wide <- page_data_tbls[[1]]
-        ## pivot long for database
-        tbl_long <- page_data_tbls[[2]]
-        write_csv(tbl_wide, paste0('data/',lmr_name_clean,"-",tbl_name_clean,".csv"))
-        write_csv(tbl_long, paste0('data/',lmr_name_clean,"-",tbl_name_clean,"_long.csv"))
-        
-        ## > ADD to existing report data ####
-        ## add to existing data from prev tables in report - depending on metric
-        if(tbl_meta[[4]]=='netsales'){
-          tables_all_netsales <- bind_rows(tables_all_netsales, tbl_long)
-        } else {
-          tables_all_litres <- bind_rows(tables_all_litres, tbl_long)
-        }
-      } ## end save & append section
+      
+      # check if page comes back with data
+      if(is.null(page_data_tbls[[1]])){
+        cat(p, "no data found on this page \n")
+      } else {
+        ## > SAVE: wide, long ####
+        ## save results for page - identifying report by name and table -> wide and long versions
+        ## confirm that table returned has data -> any more than 1 NA in first row, skip it
+        if(rowSums(is.na(page_data_tbls[[1]][1,]))<2){
+          ## as shown in report
+          tbl_wide <- page_data_tbls[[1]]
+          ## pivot long for database
+          tbl_long <- page_data_tbls[[2]]
+          ## if continued frm prev page, append
+          if(tbl_cont){
+            tbl_wide_cat <- bind_rows(tbl_wide_cat, tbl_wide)
+            tbl_long_cat <- bind_rows(tbl_long_cat, tbl_long)
+          } else {
+            ## if not continued, set/reset to new tables
+            tbl_wide_cat <- tbl_wide
+            tbl_long_cat <- tbl_long
+          }
+          
+          ## SAVE for reference
+          write_csv(tbl_wide_cat, paste0('data/',lmr_name_clean,"-",tbl_name_clean,".csv"))
+          cat(paste0('Saved: ',lmr_name_clean,"-",tbl_name_clean,"\n"))
+          write_csv(tbl_long_cat, paste0('data/',lmr_name_clean,"-",tbl_name_clean,"_long.csv"))
+          cat(paste0('Saved: ',lmr_name_clean,"-",tbl_name_clean,"_long\n"))
+          
+          ## > ADD to existing report data ####
+          ## add to existing data from prev tables in report - depending on metric
+          if(tbl_meta[[4]]=='netsales'){
+            tables_all_netsales <- bind_rows(tables_all_netsales, tbl_long)
+          } else {
+            tables_all_litres <- bind_rows(tables_all_litres, tbl_long)
+          }
+        } ## end save & append section
+      }
     } ## end of the main page loop action
-} ## end page loop
+} ## > END page loop: STOP here for basic testing of pdf-scraping ----
+
 ## 4. JOIN: netsales + litres ####
 ## - join tables with all pages/tables in each of netsales and litres as metrics
 ## - join on all fields except metrics
